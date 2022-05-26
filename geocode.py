205b875b6055ae69dfa02b1335c95adf55168fdb
Eric <89803837+ericf1@users.noreply.github.com>
2022-05-26 19:34:25 -0400

    Update geocode.py

--- geocode.py
+++ geocode.py
@@ -2,15 +2,18 @@ import time
 import requests
 import concurrent.futures
 import pandas as pd
+import math
 
 http = requests.Session()
+
+servers = ['localhost:2322']
 
 # manages the request to the localhost/server running photon.jar
 
 
-def geoRequest(loc):
+def geoRequest(loc, server):
     params = {'q': loc, 'limit': 30}
-    url = f'http://localhost:2322/api'
+    url = f'http://{server}/api'
     r = http.get(url, params=params,
                  headers={'user-agent': 'geocode-tester'})
     r = r.json()
@@ -18,9 +21,7 @@ def geoRequest(loc):
     return r
 
 
-def main():
-    # read the searches.csv (contains all the queries we need)
-    df = pd.read_csv("searches")
+def geocode(loc, server):
     # df to store the result
     addressDF = pd.DataFrame(columns=[
         'location',
@@ -41,14 +42,13 @@ def main():
     ])
     with concurrent.futures.ThreadPoolExecutor() as executor:
         # we only want the "location"
-        loc = df.loc[:, ['query']].to_numpy()
 
         # last 100 of the searches
         # on average: 100 takes around 7 seconds
-        loc = loc[-100:]
+        # loc = loc[-100:]
 
         # use an executor map
-        results = executor.map(geoRequest, loc)
+        results = executor.map(geoRequest, loc, [server for _ in loc])
         for result in results:
             for address in result['features']:
                 g = address['geometry']['coordinates']
@@ -72,6 +72,25 @@ def main():
     # save as csv
     addressDF.to_csv('addresses.csv', index=False,
                      header=True, encoding='utf-8')
+
+# spliting array into chunks:
+# https://stackoverflow.com/questions/312443/how-do-you-split-a-list-into-evenly-sized-chunks
+
+
+def chunks(lst, n):
+    return [lst[i:i + n] for i in range(0, len(lst), n)]
+
+
+def main():
+    # read the searches.csv (contains all the queries we need)
+    df = pd.read_csv("searches")
+    allLoc = df.loc[:, ['query']].to_numpy()
+    eachLocArrSize = math.ceil(float(len(allLoc)/len(servers)))
+    locSplit = chunks(allLoc, eachLocArrSize)
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        results = executor.map(geocode, locSplit, servers)
+        for result in results:
+            result
 
 
 if __name__ == '__main__':
